dd9563d1a01b6600c8bac592b2ca0cd3a2ba73a0
Peter Pharos <peter.pharos@gmail.com>
2018-02-14 19:57:25 +0000

    Still fighting with git

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 export const MatchList = () => (
   <div>
     <h1>List of Matches - Watch this space! </h1>
   </div>
 );
-=======
-export const MatchList = () => (
-  <div>
-    <h1>List of Matches - Watch this space! </h1>
-  </div>
-);
->>>>>>> 8ac8812580362e23c6ed1602d7374f60ce56c6e6
