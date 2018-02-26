a217884c6f7db129887b8e1cfb868b314b7c2e05
Peter Pharos <peter.pharos@gmail.com>
2018-02-26 18:57:41 +0000

    Finished about section

--- src/components/About.js
+++ src/components/About.js
@@ -4,24 +4,64 @@ import '../styles/styles.scss';
 import '../styles/about.scss';
 
 export class About extends Component {
+  toggleOpen(name) {
+    var text = document.getElementById(name);
+
+    text.className = text.className == 'collapsed' ? 'expanded' : 'collapsed';
+  }
+
   render() {
     return (
       <div className="about">
-        <a href="#what">
-          <h2>What</h2>{' '}
-        </a>
-        <a href="#why-short">
-          <h2> Why (short version) </h2>
-        </a>
-        <a href="#why-long">
-          <h2> Why (long version) </h2>
-        </a>
-        <a href="#who">
-          <h2> Who </h2>
-        </a>
-        <a href="#seriously">
-          <h2> This is the most pretentious thing I've ever seen! </h2>
-        </a>
+        <h2 onClick={() => this.toggleOpen('what')}>What</h2>
+        <p id="what" className="collapsed">
+          Wine and Painting Matching is what it says: a website that recommends wine and painting pairings!
+        </p>
+        <h2 onClick={() => this.toggleOpen('why-short')}> Why (short version) </h2>
+        <p id="why-short" className="collapsed">
+          Because I like paintings. And I like wine. And I find that they go well together. But mostly because it is
+          really strange to think of something and find out it is actually missing from the Web.
+        </p>
+        <h2 onClick={() => this.toggleOpen('why-long')}> Why (long version) </h2>
+        <div id="why-long" className="collapsed">
+          <p>
+            The inspiration for this website comes from a Toulouse-Lautrec anecdote: apparently, after dinner Henri used
+            to treat his friends to viewings of paintings from his collection, considering them the perfect finale to a
+            great dinner.
+          </p>
+          <p>
+            The intention here is slightly different: we've all heard of wine-and-food pairings, and you might have seen
+            the trend for wine-and-music pairings. For me, however, the latter is not a great match: while it is
+            certainly enjoyable to listen to music while drinking wine, most music changes quite quickly to be a true
+            "match." A three-minute rock song is too brief for a glass of wine; a twenty-minute classical piece will
+            change tempo and style quite a lot. As for movies and books, they require too much concentration; it seems
+            to me that one can focus on either one or the other.
+          </p>
+          <p>
+            A painting, however, is about the perfect match. Most of us tend to look at paintings for a few minutes, if
+            even that. A work of art, however, must be observed significantly longer to be truly seen. Like a glass of
+            wine, the art itself remains the same, but layers of initially hidden complexity are revealed to the
+            attentive observer. Simultaneously, the static nature of the medium means that it is easy to dip in and out
+            of the one to the other, for a truly matching multi-sensory experience.
+          </p>
+          <p>
+            The ubiquity of tablets and smartphones also means that we can enjoy a greater array of paintings than ever
+            before, without the need for heavy volumes of art encyclopedias. Where I can, I am using links from the
+            excellent Google Art project, which provides high resolution images, with good zoom in/out.
+          </p>
+          <p>So, come and join me! You might find you enjoy paintings (or wine!) more than you imagine!</p>
+        </div>
+        <h2 onClick={() => this.toggleOpen('who')}> Who </h2>
+        <p id="who" className="collapsed">
+          I like paintings. I like wine. I have no qualifications in either, so I would not recommend taking me too
+          seriously. I hope you like the idea though!
+        </p>
+        <h2 onClick={() => this.toggleOpen('seriously')}> This is the most pretentious thing I've ever seen! </h2>
+        <p id="seriously" className="collapsed">
+          You must not deal with wine match. Or art. If it's any comfort, it's not meant to be taken too seriously. The
+          main point I want to make is: "have you tried looking at paintings while drinking wine? They actually go very
+          well together!"
+        </p>
       </div>
     );
   }
--- src/components/Match.js
+++ src/components/Match.js
@@ -8,16 +8,22 @@ import '../styles/match.scss';
 import Left from 'react-icons/lib/fa/chevron-left';
 import Right from 'react-icons/lib/fa/chevron-right';
 import Facebook from 'react-icons/lib/fa/facebook';
+import WhatsApp from 'react-icons/lib/fa/whatsapp';
 import Twitter from 'react-icons/lib/fa/twitter';
 import Email from 'react-icons/lib/fa/envelope-o';
 
 import { Wine } from './Wine';
 import { Painting } from './Painting';
 
+//  Get the current url (in the future to be from the db)
+const matchUrl = 'https://www.theguardian.com/uk';
+
 export class Match extends Component {
   constructor(props) {
     super(props);
     this.state = { indexNo: 0 };
+
+    this.sendEmail = this.sendEmail.bind(this);
   }
 
   //  Load an older match
@@ -33,28 +39,45 @@ export class Match extends Component {
   }
 
   /** Shares the current match on social media. */
-  shareSocial(site) {
-    // e.preventDefault();
+  shareSocial(site, e) {
+    e.preventDefault();
 
-    //  Assign the url, again with some stupid pattern I don't get
-    var socialUrl = (function(site) {
-      switch (site) {
-        case 'fb':
-          return 'https://www.facebook.com/sharer/sharer.php?u=';
-        case 'twitter':
-          return 'https://twitter.com/share?url=';
-      }
-    })();
+    //  Assign the url
+    var socialUrl;
+
+    //  For some weird reason it doesn't get this otoh works
+    switch (site) {
+      case 'fb':
+        socialUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + matchUrl;
+        break;
+      case 'wa':
+        socialUrl = '"whatsapp://send?text=' + matchUrl;
+        break;
+      case 'twitter':
+        socialUrl = 'https://twitter.com/share?url=' + matchUrl;
+        break;
+    }
     console.log(socialUrl);
     //  Open the window
-    window.open(socialUrl + 'google.com', 'sharer', 'height=350,width=600');
+    window.open(socialUrl, 'sharer', 'height=350,width=600');
     if (window.focus) {
       window.focus();
     }
     return false;
   }
 
-  //  FROM HERE: CREATE SHARER
+  /** Opens the email client to send a link with the match. */
+  sendEmail() {
+    var eSubject = 'Wine%20and%20Painting%20Matching';
+    var eBody = 'Check%20out%20this%20wine%20and%20painting%20match!%20';
+
+    var refText = 'mailto:?to=&subject=' + eSubject + '&body=' + eBody + matchUrl;
+
+    console.log(refText);
+
+    return refText;
+  }
+
   render() {
     const entriesNo = this.props.matches.length;
 
@@ -63,13 +86,16 @@ export class Match extends Component {
         <h2 className="match">Latest Match</h2>
         <ul className="contact">
           <li>
-            <Facebook onClick={this.shareSocial('fb')} />
+            <Facebook onClick={this.shareSocial.bind(this, 'fb')} />
           </li>
           <li>
-            <Twitter onClick={this.shareSocial('twitter')} />
+            <WhatsApp onClick={this.shareSocial.bind(this, 'wa')} />
           </li>
           <li>
-            <Email />
+            <Twitter onClick={this.shareSocial.bind(this, 'twitter')} />
+          </li>
+          <li>
+            <Email onClick={this.sendEmail} />
           </li>
         </ul>
         <div>
