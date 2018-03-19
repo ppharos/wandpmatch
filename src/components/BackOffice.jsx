bd1e363ae8f2210bbfdf23c0015b372159aab6c6
Peter Pharos <peter.pharos@gmail.com>
2018-03-19 20:17:13 +0000

    Implemented logout button

--- src/components/BackOffice.jsx
+++ src/components/BackOffice.jsx
@@ -17,18 +17,30 @@ export class BackOffice extends Component {
     super(props);
     //  The state holds vars for successful login and what page to show
     this.state = { logged: success, show: '' };
+    console.log('success:' + success);
+    console.log('state:' + this.state.logged);
+    //  Bind the log in and log out handler
+    this.loggedIn = this.loggedIn.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   /** On init show the login page. */
   componentWillMount() {
-    if (this.state.logged) {
+    console.log('local storage:' + localStorage.getItem('logged'));
+    console.log('CWM state:' + this.state.logged);
+    console.log('boolean check:' + (this.state.logged == true));
+    console.log('boolean check:' + (this.state.logged == 'true'));
+    //  Unbelievably this has to be checked as a string because stupid stupid localStorage doesn't support booleans
+    if (this.state.logged == 'true') {
       this.setState({
-        show: <EntryForm />,
+        logged: true,
+        show: <EntryForm logout={this.logout} />,
       });
     } else {
       this.setState({
+        logged: false,
         //  pass the function as a param
-        show: <Login logged={this.loggedIn.bind(this)} />,
+        show: <Login logged={this.loggedIn} />,
       });
     }
   }
@@ -40,6 +52,17 @@ export class BackOffice extends Component {
     localStorage.setItem('logged', true);
 
     this.setState({ show: <EntryForm /> });
+  }
+
+  /** Logs out the user and shows the main screen. */
+  logout() {
+    // Clear the logged status
+    localStorage.setItem('logged', false);
+    //  Change the success variable
+    this.success = false;
+
+    // Change state to logged out
+    this.setState({ logged: false, show: <Login logged={this.loggedIn} /> });
   }
 
   render() {
--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -2,6 +2,7 @@
  * The entry form for new matches.
  */
 import { Component, PropTypes } from 'react';
+import { Link } from 'react-router-dom';
 import { ImageDrop } from './ImageDrop';
 import '../styles/entryform.scss';
 
@@ -14,6 +15,7 @@ export class EntryForm extends Component {
     this.state = { labelURL: '', reproURL: '' };
   }
 
+  /** On Submit, add data to the json. */
   submit(e) {
     e.preventDefault();
     console.log('name', this.refs.name.value);
@@ -27,59 +29,64 @@ export class EntryForm extends Component {
     const { name, company, location, email, message } = this.props;
 
     return (
-      <form onSubmit={this.submit}>
-        <div className="ibox">
-          <h2>Wine</h2>
-          <label htmlFor="producer"> Producer </label>
-          <input id="producer" type="text" required ref="producer" />
-          <label htmlFor="wine"> Wine </label>
-          <input id="wine" type="text" required ref="wine" />
-          <label htmlFor="vintage">Vintage </label>
-          <input id="vintage" type="text" required ref="vintage" />
-          <label htmlFor="terroir">Terroir </label>
-          <input id="terroir" type="text" required ref="terroir" />
-          <label htmlFor="label" className="top">
-            Label
-          </label>
-          <ImageDrop
-            id="label"
-            text={'Drop a wine label image or click to select a file to upload.'}
-            url={this.state.labelURL}
-          />
+      <div>
+        <div className="logout">
+          <button onClick={this.props.logout}>Logout</button>
         </div>
-        <div className="ibox">
-          <h2>Painting</h2>
-          <label htmlFor="artist">Artist </label>
-          <input id="artist" type="text" required ref="artist" />
-          <label htmlFor="work">Work </label>
-          <input id="work" type="text" required ref="work" />
-          <label htmlFor="year">Year </label>
-          <input id="year" type="number" required ref="year" />
-          <label htmlFor="mats">Materials and Dimensions </label>
-          <input id="mats" type="text" required ref="mats" />
-          <label htmlFor="venue">Currently at</label>
-          <input id="venue" type="text" required ref="venue" />
-          <label htmlFor="source">Link</label>
-          <input id="source" type="text" required ref="source" />
-          <label htmlFor="repro" className="top">
-            Pic
-          </label>
-          <ImageDrop
-            id="repro"
-            text={'Drop a painting pic or click to select a file to upload.'}
-            url={this.state.reproURL}
-          />
-        </div>
-        <div className="msgbox">
-          <label className="center" htmlFor="notes">
-            Notes
-          </label>
-          <input id="notes" type="text" ref="notes" />
-        </div>
-        <div className="submit">
-          <button>Add</button>
-        </div>
-      </form>
+        <form onSubmit={this.submit}>
+          <div className="ibox">
+            <h2>Wine</h2>
+            <label htmlFor="producer"> Producer </label>
+            <input id="producer" type="text" required ref="producer" />
+            <label htmlFor="wine"> Wine </label>
+            <input id="wine" type="text" required ref="wine" />
+            <label htmlFor="vintage">Vintage </label>
+            <input id="vintage" type="text" required ref="vintage" />
+            <label htmlFor="terroir">Terroir </label>
+            <input id="terroir" type="text" required ref="terroir" />
+            <label htmlFor="label" className="top">
+              Label
+            </label>
+            <ImageDrop
+              id="label"
+              text={'Drop a wine label image or click to select a file to upload.'}
+              url={this.state.labelURL}
+            />
+          </div>
+          <div className="ibox">
+            <h2>Painting</h2>
+            <label htmlFor="artist">Artist </label>
+            <input id="artist" type="text" required ref="artist" />
+            <label htmlFor="work">Work </label>
+            <input id="work" type="text" required ref="work" />
+            <label htmlFor="year">Year </label>
+            <input id="year" type="number" required ref="year" />
+            <label htmlFor="mats">Materials and Dimensions </label>
+            <input id="mats" type="text" required ref="mats" />
+            <label htmlFor="venue">Currently at</label>
+            <input id="venue" type="text" required ref="venue" />
+            <label htmlFor="source">Link</label>
+            <input id="source" type="text" required ref="source" />
+            <label htmlFor="repro" className="top">
+              Pic
+            </label>
+            <ImageDrop
+              id="repro"
+              text={'Drop a painting pic or click to select a file to upload.'}
+              url={this.state.reproURL}
+            />
+          </div>
+          <div className="msgbox">
+            <label className="center" htmlFor="notes">
+              Notes
+            </label>
+            <input id="notes" type="text" ref="notes" />
+          </div>
+          <div className="submit">
+            <button>Add</button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
